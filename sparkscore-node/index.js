1460004ce5760c79eac1f3107e144417971faf3b
m0r4k <mor4k@outloook.com>
2018-11-06 21:56:00 +0100

    update

--- sparkscore-node/index.js
+++ sparkscore-node/index.js
@@ -9,7 +9,7 @@ var InsightUI = function(options) {
   if (typeof options.apiPrefix !== 'undefined') {
     this.apiPrefix = options.apiPrefix;
   } else {
-    this.apiPrefix = 'insight-api-sparks';
+    this.apiPrefix = 'insight-api';
   }
   if (typeof options.routePrefix !== 'undefined') {
     this.routePrefix = options.routePrefix;
